33234ab408943e67524cad20947dbf6dfa8e6c6f
tanghulu0608 <39181819@qq.com>
2023-12-21 21:58:10 +0800

    [FIX] mrp_subcontracting: 修复委外触发的下级需求采购（申请）需求都是一个独立的补货组的问题

--- addons/mrp_subcontracting/models/stock_picking.py
+++ addons/mrp_subcontracting/models/stock_picking.py
@@ -125,15 +125,11 @@ class StockPicking(models.Model):
 
     def _prepare_subcontract_mo_vals(self, subcontract_move, bom):
         subcontract_move.ensure_one()
-        group = self.env['procurement.group'].create({
-            'name': self.name,
-            'partner_id': self.partner_id.id,
-        })
         product = subcontract_move.product_id
         warehouse = self._get_warehouse(subcontract_move)
         vals = {
             'company_id': subcontract_move.company_id.id,
-            'procurement_group_id': group.id,
+            'procurement_group_id': self.group_id.id,
             'subcontractor_id': subcontract_move.picking_id.partner_id.commercial_partner_id.id,
             'picking_ids': [subcontract_move.picking_id.id],
             'product_id': product.id,
