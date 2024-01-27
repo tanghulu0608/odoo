16b9f756e2df801cfd2c176767d098eacbfe6103
tanghulu0608 <39181819@qq.com>
2024-01-27 12:20:25 +0800

    [FIX] lead time

--- addons/mrp/models/stock_rule.py
+++ addons/mrp/models/stock_rule.py
@@ -208,15 +208,6 @@ class StockRule(models.Model):
         if not bypass_delay_description:
             delay_description.append((_('Manufacturing Lead Time'), _('+ %d day(s)', manufacture_delay)))
         if bom.type == 'normal':
-            # pre-production rules
-            warehouse = self.location_dest_id.warehouse_id
-            for wh in warehouse:
-                if wh.manufacture_steps != 'mrp_one_step':
-                    wh_manufacture_rules = product._get_rules_from_location(product.property_stock_production, route_ids=wh.pbm_route_id)
-                    extra_delays, extra_delay_description = (wh_manufacture_rules - self)._get_lead_days(product, **values)
-                    for key, value in extra_delays.items():
-                        delays[key] += value
-                    delay_description += extra_delay_description
             # manufacturing security lead time
             for comp in self.picking_type_id.company_id:
                 security_delay = comp.manufacturing_lead
